bd48834b73a12cc507eae9c45a4e838cc5faddaf
Steve Schwarz <steve@agilitynerd.com>
2020-01-17 10:11:03 -0600

    Update GA ID

--- publishconf.py
+++ publishconf.py
@@ -2,23 +2,25 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+import os
+import sys
+
+from pelicanconf import *
+
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-import os
-import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
 
-SITEURL = '/'
+SITEURL = "/"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = "UA-1127677-2"
+# DISQUS_SITENAME = ""
+GOOGLE_ANALYTICS = "UA-1127677-13"
